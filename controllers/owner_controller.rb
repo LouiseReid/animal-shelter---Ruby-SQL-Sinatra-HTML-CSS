94bacbe85d19c1fafcb82645b2db5a2576963774
LouiseReid <l_reid@hotmail.co.uk>
2017-10-14 10:36:43 +0100

    add new owner

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -1,3 +1,14 @@
 require 'sinatra'
 require 'sinatra/contrib/all'
-require_relative '../models/animal'
+require_relative '../models/owner'
+
+get '/owners' do
+  @owners = Owner.all()
+  erb ( :"owners/index" )
+end
+
+post '/owners' do
+  owner = Owner.new(params)
+  owner.save
+  redirect to("/adopted/new")
+end
